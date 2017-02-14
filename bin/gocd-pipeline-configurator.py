5e6bbcd0623fe1e57b7330c0827e095ebed3fa7e
anbazhakanselvam <anbazhakanselvam@gmail.com>
2017-02-14 18:21:19 +0530

    Update gocd-pipeline-configurator.py

--- bin/gocd-pipeline-configurator.py
+++ bin/gocd-pipeline-configurator.py
@@ -59,7 +59,7 @@ def createPipeline(pipeline):
 
     json_data = json.dumps(pipeline)
     print "Json Dump Data " + json_data
-    headers = {'Accept': 'application/vnd.go.cd.v2+json', 'Content-Type': 'application/json'}
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
     response = requests.post(create_url, data=json_data, headers=headers)
     print "Error Code " + str(response.status_code)
     if not response.status_code == 200:
